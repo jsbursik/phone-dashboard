900c8b0c159a6ef4c8d20d73d7a1d99d11c81e76
Jordan Bursik <jsbursik@gmail.com>
2025-04-13 00:32:35 -0500

    added to make sure sidebar functionality

---
+++ src/routes/phones/+page.svelte
@@ -0,0 +1,64 @@
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
+<div class="container">
+	<h2>Hello World!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt ad numquam voluptatem,
+		voluptatibus amet error voluptates architecto ullam possimus omnis nobis deleniti voluptatum
+		nostrum! Veritatis laborum placeat amet nobis?
+	</p>
+</div>
