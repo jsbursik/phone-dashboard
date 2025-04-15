f6cec6a6456c0526c1aff8eea5db75617a37aa88
Jordan Bursik <jsbursik@gmail.com>
2025-04-15 16:28:53 -0500

    broke code editor out into a component

--- src/routes/settings/phones/+page.svelte
+++ src/routes/settings/phones/+page.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
-	import { basicEditor } from 'prism-code-editor/setups';
-	import 'prism-code-editor/prism/languages/markup';
-
-	import { onMount } from 'svelte';
-
-	let container: HTMLDivElement;
-	let codeEditor: HTMLDivElement;
-
-	onMount(() => {
-		codeEditor = document.createElement('div');
-		codeEditor.id = 'code-editor';
-		container.appendChild(codeEditor);
-
-		const editor = basicEditor(
-			'#code-editor',
-			{
-				language: 'markup',
-				theme: 'github-dark'
-			},
-			() => console.log('Ready')
-		);
-	});
-
-	export const csr = false;
+	import CodeEditor from '$lib/code-editor/code-editor.svelte';
 </script>
 
-<div bind:this={container} class="container">Hello World</div>
+<div class="container">
+	<CodeEditor />
+</div>
