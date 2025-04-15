ff2ebac48c3adfc11291e8b9e4f34b9552f01f45
Jordan Bursik <jsbursik@gmail.com>
2025-04-15 16:29:01 -0500

    fixed import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	let { children } = $props();
 
-	import Sidebar from '../components/sidebar/sidebar.svelte';
+	import Sidebar from '$lib/sidebar/sidebar.svelte';
 	import '../app.css';
 </script>
 
