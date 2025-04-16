396ebafe5f69afe690fde04f6ae074da91528d8b
Jordan Bursik <jsbursik@gmail.com>
2025-04-15 21:56:44 -0500

    added another example line

--- src/lib/code-editor/example.js
+++ src/lib/code-editor/example.js
@@ -1,5 +1,6 @@
-export const txt = `
-# This is a comment
+//prettier-ignore
+export const txt = 
+`# This is a comment
 this : that
 this = that
-`;
+[ cfg:/phone/config/voip/sipAccount0.cfg,account=0;reboot=0 ]`;
