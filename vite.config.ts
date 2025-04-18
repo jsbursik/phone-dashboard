a80a1202f310f3d37ed839691daa40031a838bc7
Jordan Bursik <jsbursik@gmail.com>
2025-04-18 16:48:34 -0500

    aliasing ~ for src root

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'~': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 });
