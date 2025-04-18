965ee80ed8d20a358e6e68d143e9f5a62d5f3d1f
Jordan Bursik <jsbursik@gmail.com>
2025-04-17 22:05:42 -0500

    more clarity in file structure, designing layout

---
+++ src/routes/settings/profiles/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import ModelTable from '$lib/model-list/model-table.svelte';
+	import CodeEditor from '$lib/code-editor/code-editor.svelte';
+	import Icon from '@iconify/svelte';
+
+	let toggle = $state(false);
+</script>
+
+<div class="container">
+	<h1>Phone Profiles</h1>
+	<ModelTable />
+	<button class="btn" style="justify-self: right; margin-top: 4rem; margin-bottom: 0;"
+		>Add Profile</button
+	>
+</div>
+
+<div class={`container sliding-container ${toggle ? '' : 'hidden'}`}>
+	<CodeEditor />
+</div>
