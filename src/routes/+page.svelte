ceb0731674e629e732e98e728b6d1018aa225e35
Jordan Bursik <jsbursik@gmail.com>
2025-04-18 21:22:38 -0500

    removed test stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,3 @@
-<script lang="ts">
-	import { api } from '~/api';
-
-	async function writeProfile() {
-		console.log('Writing profile...');
-		const res = await api.profile.addProfile('test', 'This is a test file');
-		if (res.error) {
-			console.error('Error: ', res.error);
-		} else {
-			console.log('Success: ', res);
-		}
-	}
-</script>
-
-<div class="container">
-	<h2>Hello World!</h2>
-	<p>
-		<button class="btn" onclick={writeProfile}>Click me</button>
-	</p>
-</div>
-<div class="container">
-	<h2>Hello World!</h2>
-	<p style="width: 100%;">test</p>
-</div>
 <div class="container">
 	<h2>Hello World!</h2>
 	<p>
