7e8af1e7cb9b34a9649ab4213feffc3cdf7fcb36
Jordan Bursik <jsbursik@gmail.com>
2025-04-21 16:47:41 -0500

    add profile page added with variable tracking

---
+++ src/routes/settings/profiles/add_profile/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import CodeEditor from '$lib/code-editor/code-editor.svelte';
+
+	import './add_profile.css';
+
+	let varArr: string[] = [];
+</script>
+
+<div class="container-row">
+	<div class="container fit">
+		<CodeEditor bind:vars={varArr} />
+	</div>
+	<div class="container fit">
+		<h1>Variables:</h1>
+		<ul class="var-list" style="color: var(--success-clr)">
+			{#each varArr as item}
+				<li>{item}</li>
+			{/each}
+		</ul>
+	</div>
+</div>
