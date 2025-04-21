31043029fdd7cb5c4259b0007572fb20d182c5e2
Jordan Bursik <jsbursik@gmail.com>
2025-04-21 16:46:40 -0500

    tweaked regex, implemented variable tracking, etc.

--- src/lib/code-editor/code-editor.svelte
+++ src/lib/code-editor/code-editor.svelte
@@ -8,13 +8,21 @@
 
 	import { onMount } from 'svelte';
 
+	let { vars = $bindable(), ...props } = $props();
+
+	vars = [];
+
 	async function createCodeEditor() {
 		const { createEditor } = await import('prism-code-editor');
 
 		const editor = createEditor('#editor', {
 			language: 'phone_cfgs',
 			value:
-				'# Paste your config here\n# For any dynamic values use "$value" format with $kebab_case'
+				'# Paste your config here\n# For any dynamic values use "$value" format with $kebab_case\n$money\n$test',
+			onUpdate: (code) => {
+				const reg = /(?<!#.*)\$\S*/g;
+				vars = code.match(reg) as string[];
+			}
 		});
 	}
 
@@ -23,5 +31,19 @@
 	});
 </script>
 
-<h1>Phone Config</h1>
+<div class="form-group" style="margin: 1rem auto;">
+	<div class="form-row">
+		<span class="form-control">
+			<label for="model">Model:</label>
+			<input type="text" id="model" />
+		</span>
+	</div>
+</div>
+
 <div id="editor"></div>
+
+<div style="padding-top: 1rem;">
+	<div class="form-row">
+		<button class="btn btn-primary" style="margin-left: auto">Submit</button>
+	</div>
+</div>
