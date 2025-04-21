81d9b8dbbcf9bcee29f065111a02341407792957
Jordan Bursik <jsbursik@gmail.com>
2025-04-21 16:47:24 -0500

    add profile is now its own page

--- src/routes/settings/profiles/+page.svelte
+++ src/routes/settings/profiles/+page.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
 	import ModelTable from '$lib/model-list/model-table.svelte';
-	import CodeEditor from '$lib/code-editor/code-editor.svelte';
-
-	let toggle = $state(false);
 </script>
 
 <div class="container">
 	<h1>Phone Profiles</h1>
 	<ModelTable />
-	<button
-		class="btn btn-primary"
-		style="justify-self: right; margin-top: 4rem; margin-bottom: 0;"
-		onclick={() => {
-			toggle = !toggle;
-		}}>Add Profile</button
+	<a href="/settings/profiles/add_profile" class="btn btn-primary" style="margin: 4rem 0 0 auto;"
+		>Add Profile</a
 	>
-</div>
-
-<div class={`container sliding-container ${toggle ? '' : 'hidden'}`}>
-	<CodeEditor />
 </div>
