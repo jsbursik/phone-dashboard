ca525c62a15b799adc534ece29f47fb6afa6118e
Jordan Bursik <jsbursik@gmail.com>
2025-04-21 22:01:00 -0500

    placed elements I removed from code-editor

--- src/routes/settings/profiles/add_profile/+page.svelte
+++ src/routes/settings/profiles/add_profile/+page.svelte
@@ -8,7 +8,20 @@
 
 <div class="container-row">
 	<div class="container fit">
+		<div class="form-group" style="margin: 1rem auto;">
+			<div class="form-row">
+				<span class="form-control">
+					<label for="model">Model:</label>
+					<input type="text" id="model" />
+				</span>
+			</div>
+		</div>
 		<CodeEditor bind:vars={varArr} />
+		<div style="padding-top: 1rem;">
+			<div class="form-row">
+				<button class="btn btn-primary" style="margin-left: auto">Submit</button>
+			</div>
+		</div>
 	</div>
 	<div class="container fit">
 		<h1>Variables:</h1>
