1ea5e03df56045694aaa603cf951d475023608b6
Jordan Bursik <jsbursik@gmail.com>
2025-04-25 16:45:52 -0500

    made inputs a component

---
+++ src/lib/form-components/input-field.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { field = 'Input', value = $bindable(), ...props } = $props();
+
+	const fieldID = field.replace(/\s+/g, '-').toLowerCase();
+</script>
+
+<span class="form-control">
+	<label for={fieldID}>{field}:</label>
+	<input type="text" id={fieldID} name={fieldID} />
+</span>
