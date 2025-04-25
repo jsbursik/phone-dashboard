1235a8e2748c53d4de295f1f6fec11777e5557b7
Jordan Bursik <jsbursik@gmail.com>
2025-04-25 16:46:02 -0500

    made switches into components

---
+++ src/lib/form-components/switch.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { label = '', toggle, ...props } = $props();
+</script>
+
+<label class="switch">
+	<span class="label">{label}</span>
+	<input type="checkbox" onchange={toggle} />
+	<span class="slider round"></span>
+</label>
