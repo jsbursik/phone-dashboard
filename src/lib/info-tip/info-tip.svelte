b913cd64029ad5a3fc8e0dadd8be8475ab575df7
Jordan Bursik <jsbursik@gmail.com>
2025-04-28 16:46:02 -0500

    new information tooltip

---
+++ src/lib/info-tip/info-tip.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let { tooltip = 'Test Tooltip with a lot of words to see how it renders' } = $props();
+
+	import './info-tip.css';
+</script>
+
+<span class="info-tip">
+	<Icon icon="fa6-solid:circle-info" />
+	<div id="info-tip">{tooltip}</div>
+</span>
