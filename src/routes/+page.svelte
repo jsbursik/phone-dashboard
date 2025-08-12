3aaa777810dbe14477c708687493c2af155c3415
Jordan Bursik <jsbursik@gmail.com>
2025-08-11 19:00:24 -0500

    adding in code input component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+  import CodeInput from "~/components/form-components/code-input.svelte";
+</script>
+
 <div class="container-row">
   <div class="container">
     <h1>Hello World</h1>
+  </div>
+</div>
+
+<div class="container-row">
+  <div class="container">
+    <CodeInput id="test" label="Test" />
   </div>
 </div>
