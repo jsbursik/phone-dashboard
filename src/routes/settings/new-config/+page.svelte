e581a9fa4275553e15695c2a8e5800855ec2bbb5
Jordan B <jsbursik@gmail.com>
2025-08-13 12:02:18 -0500

    working on config input page

---
+++ src/routes/settings/new-config/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import CodeInput from "~/components/form-components/code-input.svelte";
+  import Input from "~/components/form-components/input.svelte";
+
+  let phoneModel = $state("");
+</script>
+
+<div class="container-row">
+  <div class="container fit">
+    <Input id="phone-model" label="" placeholder="Phone Model" bind:value={phoneModel} />
+    <CodeInput id="phone-config" label="" />
+    <div class="row g-1" style="justify-content: flex-end">
+      <button class="btn">Another File</button>
+      <button type="submit" class="btn btn-primary">Add</button>
+    </div>
+  </div>
+  <div class="container fit">
+    <h2>Variables:</h2>
+    <ul>
+      <li>Placeholder</li>
+    </ul>
+  </div>
+</div>
