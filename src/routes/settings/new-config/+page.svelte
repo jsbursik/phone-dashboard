cc3cd860e7dbe82914d725b86ff58ca6be31d26b
Jordan B <jsbursik@gmail.com>
2025-08-19 16:42:24 -0500

    Trying to tweak the styling and flow

--- src/routes/settings/new-config/+page.svelte
+++ src/routes/settings/new-config/+page.svelte
@@ -39,23 +39,23 @@
   }
 </script>
 
-<form method="POST">
+<form method="POST" id="config-form">
+  <div class="container-row center">
+    <div class="container">
+      <Input id="phone-model" label="Model:" placeholder="Model Name" required />
+    </div>
+  </div>
   {#each files as file, i}
-    <div class="container-row">
+    <div class="container-row center">
       <div class="container fit">
         {#if i >= 1}
           <button class="btn-close" onclick={() => removeFile(i)}>✕</button>
         {/if}
         <div class="row g-1" style="justify-content: center">
-          <Input
-            id={`input-${i}`}
-            label=""
-            placeholder={i == 0 ? "Phone Model" : "File Name"}
-            bind:value={file.filename}
-            error={errorMap[`input-${i}`]}
-            required
-          />
-          <InfoTip center>Use a file name syntax like 't34w.cfg'</InfoTip>
+          <Input id={`input-${i}`} label="" placeholder={"file_name.cfg"} bind:value={file.filename} error={errorMap[`input-${i}`]} required />
+          <InfoTip
+            >Use a file name syntax like 't34w.cfg'.<br /> You can also use variables in the name, like '$mac.boot' or '$MAC.boot' if it needs to be uppercase.'</InfoTip
+          >
         </div>
         <CodeInput id={`code-${i}`} label="" bind:value={file.config} />
         {#if i == files.length - 1}
@@ -65,14 +65,15 @@
           </div>
         {/if}
       </div>
-      {#if i == 0}
-        <div class="container fit">
-          <h2>Variables:</h2>
-          <ul>
-            <li>Placeholder</li>
-          </ul>
-        </div>
-      {/if}
     </div>
   {/each}
+  <div class="container fit" id="variables">
+    <h2>Variables:</h2>
+    <ul>
+      <li>Placeholder</li>
+    </ul>
+  </div>
 </form>
+
+<style>
+</style>
