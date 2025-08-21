38c13f29636114020ec3196fdc3226129549e771
Jordan B <jsbursik@gmail.com>
2025-08-21 09:24:57 -0500

    finally figured out sticky variables container.

--- src/routes/settings/new-config/+page.svelte
+++ src/routes/settings/new-config/+page.svelte
@@ -39,41 +39,60 @@
   }
 </script>
 
-<form method="POST" id="config-form">
+<form method="POST">
   <div class="container-row center">
     <div class="container">
-      <Input id="phone-model" label="Model:" placeholder="Model Name" required />
-    </div>
-  </div>
-  {#each files as file, i}
-    <div class="container-row center">
-      <div class="container fit">
-        {#if i >= 1}
-          <button class="btn-close" onclick={() => removeFile(i)}>✕</button>
-        {/if}
-        <div class="row g-1" style="justify-content: center">
-          <Input id={`input-${i}`} label="" placeholder={"file_name.cfg"} bind:value={file.filename} error={errorMap[`input-${i}`]} required />
-          <InfoTip
-            >Use a file name syntax like 't34w.cfg'.<br /> You can also use variables in the name, like '$mac.boot' or '$MAC.boot' if it needs to be uppercase.'</InfoTip
-          >
-        </div>
-        <CodeInput id={`code-${i}`} label="" bind:value={file.config} />
-        {#if i == files.length - 1}
-          <div class="row g-1" style="justify-content: flex-end">
-            <button class="btn" onclick={addFile}>Another File</button>
-            <button type="submit" class="btn btn-primary">Add Model</button>
-          </div>
-        {/if}
+      <div class="row g-1" style="justify-content: center;">
+        <Input id="phone-model" label="Model:" placeholder="Model Name" required />
+        <InfoTip>Use any format you like, it must be unique. This will be how you define what model a phone is when you add them later.</InfoTip>
       </div>
     </div>
-  {/each}
-  <div class="container fit" id="variables">
-    <h2>Variables:</h2>
-    <ul>
-      <li>Placeholder</li>
-    </ul>
+  </div>
+  <div class="grid-wrapper" style="margin-right: -20rem;">
+    {#each files as file, i}
+      <div class="container-row center" style="grid-column: 1;">
+        <div class="container fit">
+          {#if i >= 1}
+            <button class="btn-close" onclick={() => removeFile(i)}>✕</button>
+          {/if}
+          <div class="row g-1" style="justify-content: center">
+            <Input id={`input-${i}`} label="Filename:" placeholder={"file_name.cfg"} bind:value={file.filename} error={errorMap[`input-${i}`]} required />
+            <InfoTip
+              >Use a file name syntax like 't34w.cfg'.<br /> You can also use variables in the name, like '$mac.boot' or '$MAC.boot' if it needs to be uppercase.'</InfoTip
+            >
+          </div>
+          <CodeInput id={`code-${i}`} label="" bind:value={file.config} />
+          {#if i == files.length - 1}
+            <div class="row g-1" style="justify-content: flex-end">
+              <button class="btn" onclick={addFile}>Another File</button>
+              <button type="submit" class="btn btn-primary">Add Model</button>
+            </div>
+          {/if}
+        </div>
+      </div>
+    {/each}
+    <div class="container fit" id="variables">
+      <h2>Variables:</h2>
+      <ul>
+        <li>Placeholder</li>
+      </ul>
+    </div>
   </div>
 </form>
 
 <style>
+  .grid-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 20rem;
+
+    > #variables {
+      grid-column: 2;
+      grid-row: 1 / -1;
+
+      position: sticky;
+      top: 1rem;
+      margin-left: 1.5rem;
+      width: 20rem;
+    }
+  }
 </style>
