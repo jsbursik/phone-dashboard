98658619b9e4daca6d957b143e27cd5a9bc241d2
Jordan Bursik <jsbursik@gmail.com>
2025-10-17 15:45:22 -0500

    Messed around with my new input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
+<script lang="ts">
+  import CustomInput from "$lib/components/form-components/CustomInput.svelte";
+
+  function filename(v: string) {
+    const syntax = /^(\$?[\w.-]+\.\w+|\$\w+)$/;
+    return syntax.test(v);
+  }
+</script>
+
 <div class="center center-text">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
   <p>With <a href="https://github.com/jsbursik/magic-ui">Magic-UI</a></p>
+</div>
+
+<div class="center">
+  <div class="card">
+    <CustomInput type="text" name="test-input" id="test-input" placeholder="Test Input" validator={filename} required />
+  </div>
 </div>
