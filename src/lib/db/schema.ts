b5d0ebf21fafbfc3c57f91d62639747dc2a05497
Jordan Bursik <jsbursik@gmail.com>
2025-10-27 16:11:50 -0500

    can't use db helper method, redid db connection

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,3 @@
-// Import and re-export Magic UI's Better-Auth schema
-export * from "@jsbursik/magic-ui/server";
-
 // Add your custom tables here
 // Example:
 // import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
@@ -13,3 +10,16 @@ export * from "@jsbursik/magic-ui/server";
 //   content: text("content"),
 //   createdAt: timestamp("created_at").defaultNow().notNull(),
 // });
+
+// Import and re-export Magic UI's Better-Auth schema
+export * from "@jsbursik/magic-ui/server";
+import type { AdditionalFiles } from "$lib/types";
+import { pgTable, text, json } from "drizzle-orm/pg-core";
+
+export const phoneConfigs = pgTable("phone_configs", {
+  phone_model: text("phone-model").primaryKey(),
+  phone_cfg_filename: text("phone-cfg-filename").notNull(),
+  phone_cfg: text("phone-cfg").notNull(),
+  additional_files: json("additional_files").$type<AdditionalFiles[]>().notNull().default([]),
+  variables: json("variables").$type<string[]>().notNull().default([]),
+});
